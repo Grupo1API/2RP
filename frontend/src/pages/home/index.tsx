692e3e3901274ed23dee946c30e5fedec53b31b9
Vitória Moura <vim.saturnino@gmail.com>
2022-09-17 18:32:28 -0300

    feature: home temporária

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
+import './style.css';
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h2>HELLO</h2>
-        <p>
-          Cras facilisis urna ornare ex volutpat, et convallis erat elementum. Ut aliquam, ipsum
-          vitae gravida suscipit, metus dui bibendum est, eget rhoncus nibh metus nec massa.
-          Maecenas hendrerit laoreet augue nec molestie. Cum sociis natoque penatibus et magnis dis
-          parturient montes, nascetur ridiculus mus.
-        </p>
+      <div className='wrapper'>
+        <div className='home'>
+          <h2 className='titulo'>Seja bem vindo!</h2>
+          <p className='texto'>
+            Sistema de gerenciamento de horas extras, tempo de sobreaviso dos colaboradores da empresa 2RP.
+          </p>
 
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+          <img className='img-logo-2rp' src="https://www.2rpnet.com.br/assets/images/2rp-net.svg" alt="logo da empresa"/> 
+        </div>
       </div>
     );
   }
