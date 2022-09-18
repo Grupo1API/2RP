abd86ee3ea657c6307f5f46e1b6155f2b862c146
Vitória Moura <vim.saturnino@gmail.com>
2022-09-18 16:47:04 -0300

    feature: adiciona novas fncionalidaes ao menu do sistema

--- frontend/src/components/sidebar/SidebarData.tsx
+++ frontend/src/components/sidebar/SidebarData.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import * as AiIcons from 'react-icons/ai';
-import * as IoIcons from 'react-icons/io';
 import * as GoIcons from "react-icons/go";
+import * as BsIcons from "react-icons/bs";
+import * as HiIcons from "react-icons/hi";
+import * as GiIcons from "react-icons/gi";
+import * as RiIcons from "react-icons/ri";
+import * as FaIcons from "react-icons/fa";
 
 export const SidebarData = [
   {
@@ -11,27 +15,57 @@ export const SidebarData = [
     cName: 'nav-text'
   },
   {
+    title: 'Controle de Ponto',
+    path: '/controle-de-ponto',
+    icon: <BsIcons.BsFillCalendarFill />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Hora Extra',
+    path: '/hora-extra',
+    icon: <FaIcons.FaBusinessTime />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Sobreaviso',
+    path: '/sobreaviso',
+    icon: <RiIcons.RiTimeFill />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Relatórios',
+    path: '/relatorios',
+    icon: <HiIcons.HiDocument />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Autorização',
+    path: '/autorizcao',
+    icon: <GiIcons.GiStamper />,
+    cName: 'nav-text'
+  },
+  {
+    title: 'Cadastros',
+    path: '/cadastros',
+    icon: <AiIcons.AiOutlineForm />,
+    cName: 'nav-text'
+  },
+  {
     title: 'Cliente',
     path: '/cliente',
     icon: <GoIcons.GoPerson />,
     cName: 'nav-text'
   },
   {
-    title: 'Centro de Resultados',
-    path: '/squad',
-    icon: <IoIcons.IoMdPeople />,
+    title: 'Projeto',
+    path: '/projeto',
+    icon: <AiIcons.AiFillProject />,
     cName: 'nav-text'
   },
   {
-    title: 'Hora Extra',
-    path: '/messages',
-    icon: <GoIcons.GoPerson />,
-    cName: 'nav-text'
-  },
-  {
-    title: 'Support',
-    path: '/support',
-    icon: <IoIcons.IoMdHelpCircle />,
+    title: 'Configuração',
+    path: '/configuracao',
+    icon: <BsIcons.BsFillGearFill />,
     cName: 'nav-text'
   }
 ];
