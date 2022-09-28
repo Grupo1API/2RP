2dc0034e66327d5338e19858946348993a50812d
EduardoPereiraCarvalho <duedu.nda@gmail.com>
2022-09-28 09:02:47 -0300

    Tela de Login

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import Cliente from './pages/cadastro_cliente';
 import Apont_hora_extra from './pages/apont_hora_extra';
 import Apont_sobreaviso from './pages/apont_sobreaviso';
 import CentroResultado from './pages/cadastro_centro_resultado';
+import Login from './pages/login';
 import Home from './pages/home';
 
 
@@ -16,6 +17,7 @@ const Main = () => (
       <Route path="/hora-extra" element={<Apont_hora_extra />} />
       <Route path="/sobreaviso" element={<Apont_sobreaviso />} />
       <Route path="/centro-resultado" element={<CentroResultado />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </Container>
 );
