e7101d9a875e04949fb0f23494322d2eb6dd1e66
Vitária Moura <vim.saturnino@gmail.com>
2022-10-09 14:27:53 -0300

    feature: add horas-extras model

---
+++ backend/src/database/models/HoraExtraModel.ts
@@ -0,0 +1,44 @@
+import { DataTypes } from 'sequelize';
+import { db } from '../db';
+
+export const HoraExtraModel = db.define('horas-extras', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  matricula: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    unique: true
+  },
+  nome_completo: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  entrada_1: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  saida_1: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  entrada_2: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  saida_2: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  nome_gestor: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  status: {
+    type: DataTypes.ENUM({values: ['ativo', 'inativo']}),
+    defaultValue: 'ativo'
+  }
+});
