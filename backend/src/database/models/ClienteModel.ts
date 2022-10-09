debdae367adf61d52583ab8b764918a421efccc7
Vitária Moura <vim.saturnino@gmail.com>
2022-10-09 13:46:16 -0300

    feature: add client model with sequelize

---
+++ backend/src/database/models/ClienteModel.ts
@@ -0,0 +1,33 @@
+import { DataTypes } from 'sequelize';
+import { db } from '../db';
+
+enum statusType {
+  ACTIVE = 'active',
+  DISABLED = 'disabled'
+}
+
+export const ClienteModel = db.define('clientes', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  nome: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  cnpj: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  status: {
+    type: DataTypes.ENUM({values: ['ativo', 'inativo']}),
+    defaultValue: 'ativo'
+  },
+  contato: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+});
