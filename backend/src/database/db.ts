bb9df1c618bf0641b2c767af97ab2f89003eb05b
Vitária Moura <vim.saturnino@gmail.com>
2022-10-27 19:35:24 -0300

    configs db tem

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -2,13 +2,13 @@ import * as pg from 'pg';
 import { Sequelize } from 'sequelize';
 
 export const db = new Sequelize(
-  process.env.DATABASE_NAME || 'dois_rp',
-  process.env.DATABASE_USER || 'postgres',
-  process.env.DATABASE_PASS || 'postgres',
+  process.env.DATABASE_NAME || 'vfgkjuei',
+  process.env.DATABASE_USER || 'vfgkjuei',
+  process.env.DATABASE_PASS || 'nQ172B-WGZ53icOV7ILC0Qb7lbmXoctV',
   {
     dialectModule: pg,
     dialect: 'postgres',
-    host: process.env.DATABASE_HOST || 'localhost',
+    host: process.env.DATABASE_HOST || 'babar.db.elephantsql.com',
     port: 5432
   }
 );
