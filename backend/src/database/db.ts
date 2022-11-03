e90631babb7c854fd07f46bd91bdba7a0e28a13c
vitoriasaturnino <vim.saturnino@gmail.com>
2022-11-02 22:26:09 -0300

    refact: db settins

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,14 +1,20 @@
 import * as pg from 'pg';
 import { Sequelize } from 'sequelize';
+import 'dotenv/config';
+
+const database_url = process.env.DATABASE_URL;
 
 export const db = new Sequelize(
-  process.env.DATABASE_NAME || 'vfgkjuei',
-  process.env.DATABASE_USER || 'vfgkjuei',
-  process.env.DATABASE_PASS || 'nQ172B-WGZ53icOV7ILC0Qb7lbmXoctV',
+  `${database_url}`,
   {
     dialectModule: pg,
     dialect: 'postgres',
-    host: process.env.DATABASE_HOST || 'babar.db.elephantsql.com',
-    port: 5432
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
   }
 );
