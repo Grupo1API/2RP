2d1d727929d27d767fa23f7dd9da467f0a0f5357
EduardoPereiraCarvalho <duedu.nda@gmail.com>
2022-11-03 14:27:32 -0300

    Aprovação das Horas Extras/Sobreaviso

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,6 +9,7 @@ import Login from './pages/login';
 import Home from './pages/home';
 import UsuarioCreate from './pages/usuario/usuario_create';
 import UsuarioUpdate from './pages/usuario/usuario_update';
+import Aprov_hora_extra from './pages/aprov_hora_extra';
 
 
 const Main = () => (
@@ -17,6 +18,7 @@ const Main = () => (
       <Route path="/" element={<Home />} />
       <Route path="/cliente" element={<Cliente />} />
       <Route path="/hora-extra" element={<Apont_hora_extra />} />
+      <Route path="/aprov-hora-extra" element={<Aprov_hora_extra />} />
       <Route path="/sobreaviso" element={<Apont_sobreaviso />} />
       <Route path="/centro-resultado" element={<CentroResultado />} />
       <Route path="/login" element={<Login />} />
