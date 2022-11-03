c1d820a08b6de94a40c6c9e2e0d9c21683c202a3
vitoriasaturnino <vim.saturnino@gmail.com>
2022-11-02 22:20:13 -0300

    feature: add actions singup and login

--- backend/src/controller/UsuariosController.ts
+++ backend/src/controller/UsuariosController.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import * as bcrypt from 'bcrypt'
+import * as jwt from "jsonwebtoken";
 import UsuariosModel from '../models/UsuariosModel';
 
 class UsuariosController {
@@ -43,6 +45,73 @@ class UsuariosController {
     await UsuariosModel.update({status: 'inativo'}, {where: {id: usuarioId }});
 
     return res.status(201).send();
+  }
+
+  async singup(req: Request, res: Response) {
+    try {
+      const { id, role, email, senha } = req.body;
+    
+      type Data = {
+        id: Number;
+        role: String;
+        email: String;
+        senha: String;
+      }
+    
+       const data: Data = {
+        id,
+        role,
+        email,
+        senha: await bcrypt.hash(senha, 10),
+       };
+
+       const user = await UsuariosModel.create(data);
+    
+       if (user) {
+         let token = jwt.sign({ id: user.id }, "lbkjbefkjbwekfkewfk", {
+           expiresIn: 1 * 24 * 60 * 60 * 1000,
+         });
+    
+         res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
+         console.log("user", JSON.stringify(user, null, 2));
+         console.log(token);
+
+         return res.status(201).send(user);
+       } else {
+         return res.status(409).send("Details are not correct");
+       }
+     } catch (error) {
+       console.log(error);
+     }
+  }
+    
+  async login(req: Request, res: Response) {
+    try {
+    const { email, senha } = req.body;
+    const user = await UsuariosModel.findOne({ email });
+
+    if (user) {
+      const isSame = await bcrypt.compare(senha, user.senha);
+  
+      if (isSame) {
+        let token = jwt.sign({ id: user.id }, "lbkjbefkjbwekfkewfk", {
+          expiresIn: 1 * 24 * 60 * 60 * 1000,
+        });
+
+        res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
+        console.log("user", JSON.stringify(user, null, 2));
+        console.log(token);
+        
+        return res.status(201).send(user);
+      } else {
+        return res.status(401).send("Authentication failed");
+      }
+      } else {
+        return res.status(401).send("Authentication failed");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
