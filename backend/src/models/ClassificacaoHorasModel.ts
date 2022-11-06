9783f8f1644c07941ca403aa02f25bf99198ec52
JulianaMaria-Lab <juliana.ramos01@fatec.sp.gov.br>
2022-11-06 02:48:56 -0300

    add classificação de horas

---
+++ backend/src/models/ClassificacaoHorasModel.ts
@@ -0,0 +1,27 @@
+import { DataTypes } from "sequelize";
+import { db } from "../database/db";
+import VerbasModel from "./VerbasModel";
+import ApontamentoHorasModel from "./ApontamentoHorasModel";
+
+const ClassificacaoHorasModel = db.define("ClassificacaoHoras", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  quantidadeHoras: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.ENUM({ values: ["ativo", "inativo"] }),
+    allowNull: false,
+    defaultValue: "ativo",
+  },
+});
+
+VerbasModel.belongsTo(ClassificacaoHorasModel);
+ApontamentoHorasModel.belongsTo(VerbasModel);
+
+export default ClassificacaoHorasModel;
